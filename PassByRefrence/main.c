041c66e61c1b0bc834b136aa3699626caf3ba424
ammar falmban <devAspirant@gmail.com>
2014-03-19 14:08:43 +0900

    الوصول الى الدوال

--- PassByRefrence/main.c
+++ PassByRefrence/main.c
@@ -7,12 +7,20 @@
 //
 
 #include <stdio.h>
-
+#include <math.h>
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    printf("Hello, World!\n");
+    // Define
+    double pi = 3.14;
+    double integerPart;
+    double fractionPart;
+    
+    // Pass the Address of integer part as an argument
+    fractionPart = modf(pi, &integerPart);
+    
+    // Find the value stored in intergerPart
+    printf("integer part = %0.f ,  fractionPart = %.2f\n",integerPart,fractionPart);
+   
     return 0;
 }
 
